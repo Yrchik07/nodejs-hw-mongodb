4dbf4132a49bf1f9b0be91f46c63df19fd1028d5
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-05-27 23:17:35 +0300

    correction of the path to contacts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-// import { Contact } from './db/models/contact.js';
 import {setupServer} from './server.js';
 
 (async () => {
     await initMongoConnection();
-    // const contacts = await Contact.find({});
-    // console.log(contacts);
     setupServer();
 })();
 
