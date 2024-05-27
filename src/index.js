bb81ef5d3842b6db1695c9db2a53411f7a3cd484
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-05-27 22:59:30 +0300

    test finish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
+// import { Contact } from './db/models/contact.js';
 import {setupServer} from './server.js';
 
 (async () => {
     await initMongoConnection();
+    // const contacts = await Contact.find({});
+    // console.log(contacts);
     setupServer();
 })();
 
