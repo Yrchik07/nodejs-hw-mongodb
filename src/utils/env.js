cb859d7a795d4a1efdbaad2dd43b2d6a7400a480
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-07-09 08:42:52 +0300

    ${envName}

--- src/utils/env.js
+++ src/utils/env.js
@@ -5,5 +5,5 @@ export const env = (envName, defaultValue) => {
     if (process.env[envName]) return process.env[envName];
     if (defaultValue) return defaultValue;
 
-    throw new Error(`Env var with name ${envName} is not found`);
+    throw new Error(`Env var with name [${envName}] is not found`);
   };
